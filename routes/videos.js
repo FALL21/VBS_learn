8fc8a15446fc884b5641a451ef1bbbfba34cbd0c
mame bou fall <mameboufall21@gmail.com>
2022-11-17 17:19:11 +0000

    ...

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,17 @@
 import express from "express";
-import { } from "../controllers/video.js";
+import { addVideo, addView, deleteVideo, getVideo, random, trend, updateVideo } from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+//create a video
+router.post("/", verifyToken, addVideo )
+router.put("/:id", verifyToken, updateVideo )
+router.delete("/:id", verifyToken, deleteVideo )
+router.get("/find/:id", verifyToken, getVideo )
+router.put("/view/:id", addView )
+router.get("/trend", trend )
+router.get("/random",random )
+router.get("/sup", sup )
 
 export default router; 
